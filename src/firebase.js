1ff26f3bce4621701376e13b2814f08a97709c2e
Developer <dev@local.machine>
2026-04-01 19:57:05 -0300

    Adicionando chaves reais do Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore, collection, addDoc, onSnapshot, query, where, orderBy } f
 
 // TODO: Substitua pelos dados do seu Console do Firebase (firebase.google.com)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyC9-3flNhLog1OGvfvJdlaK2GRUDFRXPhQ",
+  authDomain: "voice-notes-app-3b5d7.firebaseapp.com",
+  projectId: "voice-notes-app-3b5d7",
+  storageBucket: "voice-notes-app-3b5d7.firebasestorage.app",
+  messagingSenderId: "879028812746",
+  appId: "1:879028812746:web:1ad62dca3cf44568a36d33",
+  measurementId: "G-7TW1Z5VL49"
 };
 
 const app = initializeApp(firebaseConfig);
